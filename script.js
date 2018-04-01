45810ae166b1a2258615c777308928705bba8aac
Victor Lee <myleevictor@gmail.com>
2018-04-01 23:22:01 +0800

    added tests and .gitignore

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function sumTwoSmallestNumbers(numbers) {
   const sortedArr = numbers.sort((a, b) => {
-    a - b;
+    return a - b;
   });
   const filteredArr = sortedArr.filter(elem => elem > 0);
   return filteredArr[0] + filteredArr[1];
 }
-console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]));
+
+module.exports = sumTwoSmallestNumbers;
--- script.test.js
+++ script.test.js
@@ -1,29 +1,19 @@
-describe("Your function", _ => {
-  it("should work for basic tests", _ => {
-    Test.assertEquals(
-      sumTwoSmallestNumbers([5, 8, 12, 19, 22]),
-      13,
-      "Sum should be 13"
-    );
-    Test.assertEquals(
-      sumTwoSmallestNumbers([15, 28, 4, 2, 43]),
-      6,
-      "Sum should be 6"
-    );
-    Test.assertEquals(
-      sumTwoSmallestNumbers([3, 87, 45, 12, 7]),
-      10,
-      "Sum should be 10"
-    );
-    Test.assertEquals(
-      sumTwoSmallestNumbers([23, 71, 33, 82, 1]),
-      24,
-      "Sum should be 24"
-    );
-    Test.assertEquals(
-      sumTwoSmallestNumbers([52, 76, 14, 12, 4]),
-      16,
-      "Sum should be 16"
-    );
+const sumTwoSmallestNumbers = require("./script");
+
+describe("Function tests", () => {
+  it("sumTwoSmallestNumbers([5, 8, 12, 19, 22]) should return 13", () => {
+    expect(sumTwoSmallestNumbers([5, 8, 12, 19, 22])).toEqual(13);
+  });
+
+  it("sumTwoSmallestNumbers([15, 28, 4, 2, 43]) should return 6", () => {
+    expect(sumTwoSmallestNumbers([15, 28, 4, 2, 43])).toEqual(6);
+  });
+
+  it("sumTwoSmallestNumbers([3, 87, 45, 12, 7]) should return 10", () => {
+    expect(sumTwoSmallestNumbers([3, 87, 45, 12, 7])).toEqual(10);
+  });
+
+  it("sumTwoSmallestNumbers([23, 71, 33, 82, 1]) should return 24", () => {
+    expect(sumTwoSmallestNumbers([23, 71, 33, 82, 1])).toEqual(24);
   });
 });
